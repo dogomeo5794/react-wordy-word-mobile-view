8d22fa50ff20d8d78f5cc1f741dfd7fcdff30960
ronald dogomeo <ronald.dogomeo@upteamco.com>
2020-06-16 18:04:34 +0800

    initial commit

--- src/layouts/Field.js
+++ src/layouts/Field.js
@@ -2,54 +2,50 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
-const useStyles = theme => ({
+const useStyles = (theme) => ({
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: "25ch",
-    textTransform: 'uppercase'
+    textTransform: "uppercase",
   },
 });
 
 export class LayoutTextFields extends Component {
 
-    render() {
-        const { classes, handleChange, words, detectKey } = this.props;
+  render() {
+    // const { classes, handleChange, words, detectKey } = this.props;
+    const { handleChange, words, detectKey } = this.props;
 
-  return (
-      
-    <div>
-      <TextField
-        id="outlined-full-width"
-        label="Input Random Letters"
-        style={{ margin: 8 }}
-        // placeholder="Placeholder"
-        // helperText="Full width (this is helper text)!"
-        fullWidth
-        margin="normal"
-        // InputLabelProps={{
-        // //   shrink: true,
-        // }}
-        inputProps = {{
+    return (
+      <div>
+        <TextField
+          id="outlined-full-width"
+          label="Input Random Letters"
+          style={{ margin: 8 }}
+          // placeholder="Placeholder"
+          // helperText="Full width (this is helper text)!"
+          fullWidth
+          margin="normal"
+          // InputLabelProps={{
+          // //   shrink: true,
+          // }}
+          inputProps={{
             maxLength: 7,
             min: 1,
             style: {
-                textTransform: 'uppercase',
-                letterSpacing: '10px'
-            }
-        }}
-        value={words}
-
-        onChange={handleChange}
-
-        onKeyUp={detectKey}
-        
-        variant="outlined"
-      />
-    </div>
-  
-  );
-    }
+              textTransform: "uppercase",
+              letterSpacing: "10px",
+            },
+          }}
+          value={words}
+          onChange={handleChange}
+          onKeyUp={detectKey}
+          variant="outlined"
+        />
+      </div>
+    );
+  }
 }
 
-export default withStyles(useStyles)(LayoutTextFields)
+export default withStyles(useStyles)(LayoutTextFields);
--- src/layouts/MainBody.js
+++ src/layouts/MainBody.js
@@ -8,11 +8,18 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import TextFormat from "@material-ui/icons/TextFormat";
 import ListItemText from "@material-ui/core/ListItemText";
-import DraftsIcon from "@material-ui/icons/Drafts";
-import Paper from '@material-ui/core/Paper';
+// import DraftsIcon from "@material-ui/icons/Drafts";
+import Avatar from "@material-ui/core/Avatar";
+import Paper from "@material-ui/core/Paper";
+
+// import MenuItem from '@material-ui/core/MenuItem';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import FormControl from '@material-ui/core/FormControl';
+// import Select from '@material-ui/core/Select';
+
 
 // import english from "../externalJS/english_words"
-import english from "./words_dictionary.json"
+import english from "./words_dictionary.json";
 
 const useStyles = (theme) => ({
   root: {
@@ -39,58 +46,83 @@ const useStyles = (theme) => ({
     padding: 14,
   },
   uppercase: {
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    width: '100%'
+    textAlign: "center",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    width: "100%",
   },
   lowercase: {
-    width: '100%',
-  }
+    width: "100%",
+  },
+  custom: {
+    border: "1px solid grey",
+    borderRadius: "5px",
+    margin: "3px 0",
+    textTransform: "uppercase",
+  },
+  letterSpacing: {
+    letterSpacing: "4px",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 });
-
-function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
 
 export class MainBody extends Component {
   state = {
     words: "",
-    final_words: '',
-    random: ['1', '2', '3']
+    final_words: "",
+    random: [],
+    charLen: [],
+    charLenSelected: 3
   };
 
   detectKey = (e) => {
     if (e.keyCode === 13) {
-        // console.log(english);
-        this.setState( {
-            final_words: this.state.words.split('').join(' - '),
-        } )
-        console.log(this.state.words);
-        let joined = this.tree(this.state.words.split('')).map(function(str) {
-            return str.join('');
-        });
+      // console.log(english);
+      this.setState({
+        final_words: this.state.words.split("").join(" - "),
+      });
 
-        let words = joined.filter(function(item, pos, self) {
-            return self.indexOf(item) == pos && item in english && item.length > 3;
-        });
+      let arr = [];
+      for ( var i = 3; i <= this.state.words.length; i++ ) {
+        arr.push(i)
+      }
         this.setState( {
-            random: words
+            charLen: arr
         } )
+      
+      console.log(this.state.words);
+      let joined = this.tree(this.state.words.split("")).map(function (str) {
+        return str.join("");
+      });
+
+      let words = joined.filter(function (item, pos, self) {
+        return self.indexOf(item) === pos && item in english && item.length >= 3;
+      });
+      this.setState({
+        random: words,
+      });
     }
   };
 
   tree = (leafs) => {
-        var branches = [];
-        if (leafs.length == 1) return leafs;
-        for (var k in leafs) {
-            var leaf = leafs[k];
-            this.tree(leafs.join('').replace(leaf, '').split('')).concat("").map(function(subtree) {
-                branches.push([leaf].concat(subtree));
-            });
-        }
-        return branches;
-  }
+    var branches = [];
+    if (leafs.length === 1) return leafs;
+    for (let k in leafs) {
+      let leaf = leafs[k];
+      this.tree(leafs.join("").replace(leaf, "").split(""))
+        .concat("")
+        .map(function (subtree) {
+          return branches.push([leaf].concat(subtree));
+        });
+    }
+    return branches;
+  };
 
   handleChange = (e) => {
     let value = e.target.value;
@@ -100,57 +132,94 @@ export class MainBody extends Component {
     this.setState({
       words: value,
     });
+
   };
+
+  clickedNext = (e) => {
+    this.setState( {
+        words: "",
+        final_words: "",
+        random: [],
+        charLen: [],
+        charLenSelected: 3
+    } )
+  }
+
+  selectLengthChange = (e) => {
+      this.setState( {
+        charLenSelected: e.target.value
+      } );
+  }
+
+
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.root} style={{ marginTop: 70 }} >
+      <div className={classes.root} style={{ marginTop: 70 }}>
         <Field
           handleChange={this.handleChange}
           words={this.state.words}
           detectKey={this.detectKey}
         />
 
-        <Paper className={classes.words_random}>
-            {(() => {
-                if (this.state.final_words) {
-                    return (<span className={classes.uppercase}>{this.state.final_words}</span>)
-                } else {
-                    return (<span className={classes.lowercase}>Display random words here!</span>)
-                }
-            })()}
-        </Paper>
+        {this.state.final_words &&
+            <Paper className={classes.words_random}>
+                <span className={classes.uppercase}>
+                    {this.state.final_words}
+                </span>
+            </Paper>
+        }
+
+        {/* <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel id="demo-simple-select-outlined-label">Select Character Length</InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            // value={this.state.charLenSelected}
+            // onChange={this.selectLengthChange}
+            label="Select Character Length"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+
+            {this.state.charLen.map((value, index) => {
+                console.log(value)
+                // return (
+                //     <MenuItem value={value}>{value} Character</MenuItem>
+                // )
+            })}
+            
+            
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl> */}
 
         <List component="nav" aria-label="main mailbox folders">
-          <ListItem button>
-            <ListItemIcon>
-              <TextFormat />
-            </ListItemIcon>
-            <ListItemText primary="Inbox" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
-          </ListItem>
-            {this.state.random.map((value, index) => {
-                // return <li key={index}>{value}</li>
-                return (
-                    <ListItem button key={index}>
-                        <ListItemIcon>
-                            <TextFormat />
-                        </ListItemIcon>
-                        <ListItemText primary={value} />
-                    </ListItem>
-                )
-            })}
+          {this.state.random.map((value, index) => {
+            // return <li key={index}>{value}</li>
+            return (
+              <ListItem className={classes.custom} button key={index}>
+                <ListItemIcon>
+                  <Avatar>
+                    <TextFormat />
+                  </Avatar>
+                </ListItemIcon>
+                <ListItemText
+                  classes={{ primary: classes.letterSpacing }}
+                  primary={value}
+                />
+              </ListItem>
+            );
+          })}
         </List>
 
-        <Fab variant="extended" className={classes.absolute}>
+        <Fab variant="extended" onClick={this.clickedNext} className={classes.absolute}>
           NEXT
           <ArrowForward className={classes.extendedIcon} />
         </Fab>
+
       </div>
     );
   }
